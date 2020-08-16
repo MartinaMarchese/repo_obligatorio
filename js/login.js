1789d8b5f7eff8e01b98a9b6530e9652cdc19212
Martina <martu97214@gmail.com>
2020-08-16 15:04:59 -0300

    Cambios parte 1

--- js/login.js
+++ js/login.js
@@ -2,5 +2,18 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    
+        
 
 });
+
+function validate(){
+    var username = document.getElementById("inputEmail").value;
+    var password = document.getElementById("inputPassword").value;
+    if ( username == "marmar" && password == "marmar123"){
+    alert ("Login successfully");
+    window.location = "index.html"; // Redirecting to other page.
+    return false;
+    }
+    
+    }
